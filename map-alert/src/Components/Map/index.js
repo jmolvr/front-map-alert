d997fb8706d72e4d44dc19491104169115efb8cd
Marcos Oliveira <marcos.abreu.oliveira@gmail.com>
2019-11-20 11:32:36 -0300

    Adicionando modal de detalhes do alerta

--- map-alert/src/Components/Map/index.js
+++ map-alert/src/Components/Map/index.js
@@ -1,36 +1,58 @@
 import React from "react";
 import styles from "./style";
-import { Platform } from "react-native";
+import { Platform, View, Text } from "react-native";
 import MapView, { Marker, UrlTile, MAP_TYPES } from "react-native-maps";
-import { connect } from 'react-redux';
-
+import { connect } from "react-redux";
+import Modal from "react-native-modal";
 
 class Map extends React.Component {
+  state = {
+    activeModal: false
+  };
+
   render() {
     const { alerts, region } = this.props;
 
     return (
-      <MapView
-        style={styles.map}
-        region={region}
-        provider={null}
-        mapType={Platform.OS == "android" ? MAP_TYPES.NONE : MAP_TYPES.STANDARD}
-        rotateEnabled={false}
-        showsUserLocation
-      >
-        <UrlTile urlTemplate="http://c.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <View>
+        <MapView
+          style={styles.mapStyle}
+          region={region}
+          provider={null}
+          mapType={
+            Platform.OS == "android" ? MAP_TYPES.NONE : MAP_TYPES.STANDARD
+          }
+          rotateEnabled={false}
+          showsUserLocation
+        >
+          <UrlTile urlTemplate="http://c.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-        {alerts.map(alert => (
-          <Marker
-            key={alert.id}
-            coordinate={{
-              latitude: alert.latitude,
-              longitude: alert.longitude
-            }}
-            title={alert.descricao}
-          />
-        ))}
-      </MapView>
+          {alerts.map(alert => (
+            <Marker
+              onPress={() => this.setState({ activeModal: true })}
+              key={alert.id}
+              coordinate={{
+                latitude: alert.latitude,
+                longitude: alert.longitude
+              }}
+              title={alert.descricao}
+            />
+          ))}
+        </MapView>
+
+        <Modal
+          isVisible={this.state.activeModal}
+          useNativeDriver
+          style={styles.modalContainer}
+          onBackButtonPress={() => this.setState({ activeModal: false })}
+          onBackdropPress={() => this.setState({ activeModal: false })}
+          onSwipeComplete={() => this.setState({ activeModal: false })}
+        >
+          <View style={styles.modal}>
+            <Text>Ola mundo</Text>
+          </View>
+        </Modal>
+      </View>
     );
   }
 }
--- map-alert/src/Components/Map/style.js
+++ map-alert/src/Components/Map/style.js
@@ -1,13 +1,22 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
 const styles = StyleSheet.create({
-  map: {
-    flex: 3
-  },
   loadingMap: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height
+  },
+  modalContainer: {
+    margin: 0,
+    justifyContent: "flex-end"
+  },
+  modal: {
+    backgroundColor: "#ffffff",
+    height: 300
   }
 });
 
