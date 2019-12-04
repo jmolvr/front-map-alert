74e26aea6f6822fb3fdaab450eb21b6d8b40addc
Marcos Oliveira <marcos.abreu.oliveira@gmail.com>
2019-12-04 16:34:01 -0300

    Mundando botao de adicionar alerta

--- map-alert/src/Components/FabAdicionar/index.js
+++ map-alert/src/Components/FabAdicionar/index.js
@@ -1,27 +1,7 @@
 import React from "react";
-import { Portal, FAB } from "react-native-paper";
+import { FAB } from "react-native-paper";
+import styles from "./style";
 
-export default class FabAdicionar extends React.Component {
-  state = {
-    fabOpen: false
-  };
-
-  render() {
-    return (
-      <Portal>
-        <FAB.Group
-          open={this.state.fabOpen}
-          icon={"plus"}
-          actions={[
-            {
-              icon: "map-marker-plus",
-              label: "Adicionar Alerta",
-              onPress: this.props.buttonAddAlerta
-            }
-          ]}
-          onStateChange={({ open }) => this.setState({ fabOpen: open })}
-        />
-      </Portal>
-    );
-  }
+export default function FabAdicionar(props) {
+  return <FAB style={styles.fab} icon="plus" onPress={props.buttonAddAlerta} />;
 }
